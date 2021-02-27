da40ef6cfb544e97b289bd22396ee3f7678919cb
Erick <libertti@live.com>
2021-02-27 20:13:54 -0300

    Criando novos componente Profile, Countdown e CompletedChallenges

---
+++ src/components/CompletedChanllenges.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/components/CompletedChallenges.module.css'
+
+export function CompletedChanllenges(){
+
+return(
+    <div className={styles.completedChallengesComponents}>
+        <span> Desafios completos</span>
+        <span>5</span>
+    </div>
+);
+
+}
